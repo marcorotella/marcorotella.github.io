d782d17e04e1e4faa9408c170dd05fd65af781fc
Marco Rotella <marco@MBP-di-Marco.homenet.telecomitalia.it>
2023-11-19 15:51:42 +0100

    first website release

---
+++ script.js
@@ -0,0 +1,9 @@
+// This script will be for smooth scrolling to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
