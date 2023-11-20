23e685014b574341011e7697be9bd1206f8240ea
Marco Rotella <marco@MacBook-Pro-di-Marco.local>
2023-11-20 11:56:22 +0100

    refactoring HTML + email obfuscation

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // This script will be for smooth scrolling to sections
+var encEmail = "bWFyY28ucm90ZWxsYUBnbWFpbC5jb20=";
+const form = document.getElementById("contact");
+form.setAttribute("href", "mailto:".concat(atob(encEmail)));
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
